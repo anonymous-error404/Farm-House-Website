91affce192d107150c65d7b110b558703f66d3fb
Manas <guravmanas22@gmail.com>
2025-06-21 00:25:02 +0530

    created otp based verification with django core cache feature(brother django is like spring boot for python)

--- FarmHouse_Website_Backend-main/FarmHouse_Website/urls.py
+++ FarmHouse_Website_Backend-main/FarmHouse_Website/urls.py
@@ -1,12 +1,15 @@
-from FarmHouse_Website import views
+from .views import *
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'bookings', views.BookingViewSet)
-router.register(r'menu', views.MenuViewSet)
-router.register(r'reviews', views.ReviewsViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'menu', MenuViewSet)
+router.register(r'reviews', ReviewsViewSet)
+
+baseUrl = 'NirmalFarms/api'
+
 urlpatterns = [
-    path('NirmalFarms/', views.Home.as_view(), name='home'),
-    path('NirmalFarms/api/', include(router.urls)),
+    path(f'{baseUrl}/', include(router.urls)),
+    path(f'{baseUrl}/otpverification/', Authorization.as_view(), name="otp_verification")
 ]
--- FarmHouse_Website_Backend-main/FarmHouse_Website/views.py
+++ FarmHouse_Website_Backend-main/FarmHouse_Website/views.py
@@ -1,25 +1,10 @@
 from datetime import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from FarmHouse_Website.serializer import *
-
-temporary_otp_storage = {}
-class Home(APIView):
-    def get(self, request):
-        image_bin = Bookings.objects.get(bookingId=10).IDimage
-        image = base64.b64encode(image_bin).decode('utf-8')
-        html = f"""
-        <html>
-        <body>
-            <h2>ID Image Preview</h2>
-            <img src="data:image/jpeg;base64,{image}" height="1000" width="1200">
-        </body>
-        </html>
-        """
-
-        return HttpResponse(html, content_type='text/html')
+from django.core.cache import cache
 
 #gotta create a new url mapping for otp verification and call the bookingviewset.create method after otp is verified
 
@@ -44,15 +29,10 @@ class BookingViewSet(viewsets.ModelViewSet):
                 else:
                     if 'IDimage' not in serializer.validated_data:
                         return Response({'message': 'IDimage is required'}, status=status.HTTP_206_PARTIAL_CONTENT)
-                    else:
-                        receiver = serializer.validated_data['guestEmail']
-                        request.session['receiver'] = receiver
-                        if not utils.sendOtpVerificationMail(receiver) :
-                            return Response(status=status.HTTP_424_FAILED_DEPENDENCY)
-                        else :
-                            serializer.validated_data['bookingDate'] = datetime.today()
-                            serializer.save()
-                            return Response(status=status.HTTP_200_OK)
+                    else :
+                        serializer.validated_data['bookingDate'] = datetime.today()
+                        serializer.save()
+                        return Response(status=status.HTTP_200_OK)
             else:
                 return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
         else:
@@ -71,15 +51,16 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 
         if serializer.is_valid(raise_exception=True):
 
-            if not Bookings.objects.filter(bookingId=serializer.validated_data['bookingId']).exists():
+            if not Bookings.objects.filter(bookingId=Bookings.objects.get(guestPhone=request.data['guestPhone']).bookingId).exists():
                 return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
             try:
+                serializer.validated_data['bookingId'] = Bookings.objects.get(guestPhone=request.data['guestPhone']).bookingId
                 serializer.validated_data['reviewDate'] = datetime.today()
                 saved_review = serializer.save()
 
                 if utils.setMedia(media_list=request.FILES.getlist('media_list'),
-                                  review=Reviews.objects.get(reviewId=saved_review.reviewId)):
+                                review=Reviews.objects.get(reviewId=saved_review.reviewId)):
                     return Response(status=status.HTTP_200_OK)
                 else:
                     return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -104,4 +85,30 @@ class ReviewsViewSet(viewsets.ModelViewSet):
         return Response(data=review, status=status.HTTP_200_OK)
 
 
-
+class Authorization(APIView):
+    
+    def get(self, request):
+        
+        if request.data['guestEmail'] :
+            receiver = request.data['guestEmail']
+            
+            if Bookings.objects.filter(guestEmail=receiver).exists() :
+                return Response(status=status.HTTP_100_CONTINUE)
+            elif utils.sendOtpVerificationMail(receiver=receiver):
+                request.session['customerEmail']=receiver
+                return Response(status=status.HTTP_200_OK)
+            else :
+                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+        
+    def post(self,request):
+        
+        if request.session['customerEmail'] and request.data['otp']:
+            customerEmail = request.session['customerEmail']
+            otp = request.data['otp']
+            
+            if cache.get(customerEmail) == otp :
+                cache.delete(customerEmail)
+                return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response(status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
