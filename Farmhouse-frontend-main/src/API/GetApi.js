c306c816ed97019795e7636a04ca0b179c9c3a68
Anisha2704 <anishamalore45@gmail.com>
2025-06-22 08:48:31 +0530

    corrected baseUrl

--- Farmhouse-frontend-main/src/API/GetApi.js
+++ Farmhouse-frontend-main/src/API/GetApi.js
@@ -2,7 +2,8 @@ import axiosInstance from "./Axios";
 
 const fetchData = async () => {
   try {
-    const response = await axiosInstance.get('/menu');
+    const response = await axiosInstance.get('/reviews');
+    console.log("Data fetched successfully:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
--- Farmhouse-frontend-main/src/components/Test.jsx
+++ Farmhouse-frontend-main/src/components/Test.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Test = () => {
 
     useEffect(() => {
-     fetchData();
+    const res = fetchData();
     }, []);
   return (
     <div>
